ece5d3336dc053a364f031eb36d24331374a23d0
DYeremiia <109411139+ookioy@users.noreply.github.com>
2026-04-27 10:01:46 +0300

    Update Stryker configuration for mutation testing

--- frontend/stryker.config.mjs
+++ frontend/stryker.config.mjs
@@ -1,23 +1,25 @@
 /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
 const config = {
-    testRunner: "jest",
-    jest: {
-        configFile: "jest.config.js",
-        config: {
-            testMatch: ["**/src/helper/**/*.test.js"],
-            testPathIgnorePatterns: ["setLink"],
-            coverageThreshold: undefined
-        }
-    },
-    mutator: {
-        plugins: [],
-        excludedMutations: []
-    },
-    mutate: ["src/helper/*.js", "!src/helper/*.test.js", "!src/helper/setLInk.js"],
-    reporters: ["html", "clear-text", "progress"],
-    htmlReporter: {
-        fileName: "reports/mutation/mutation.html"
-    },
-    coverageAnalysis: "perTest"
+  testRunner: "jest",
+  jest: {
+    configFile: "jest.config.js",
+    config: {
+      testMatch: ["**/src/helper/**/*.test.js"],
+      coverageThreshold: undefined,
+    }
+  },
+  mutate: [
+    "src/helper/getHref.js",
+    "src/helper/search.js"
+  ],
+  mutator: {
+    plugins: [],
+    excludedMutations: []
+  },
+  reporters: ["html", "clear-text", "progress"],
+  htmlReporter: {
+    fileName: "reports/mutation/mutation.html"
+  },
+  coverageAnalysis: "perTest"
 };
 export default config;
